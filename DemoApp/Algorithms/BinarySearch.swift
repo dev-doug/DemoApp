efe997a28ee34c587fe6ec7a0f68aa62e5d586fa
dwall85 <dougwall85@gmail.com>
2017-12-20 11:13:35 -0500

    Completed recursive and iterative binary search, and expanded unit tests Unit Tests Passing

--- DemoApp/Algorithms/BinarySearch.swift
+++ DemoApp/Algorithms/BinarySearch.swift
@@ -10,12 +10,39 @@ import Foundation
 
 struct BinarySearch {
 
-    static func indexOfRecursive<T: Comparable>(sortedArray: [T], searchValue: T) -> Int? {
+    static func indexOfRecursive<T: Comparable>(sortedArray: [T], searchValue: T, range: Range<Int>? = nil) -> Int? {
+        let range = range ?? 0..<sortedArray.count
 
-        return nil
+        guard range.lowerBound < range.upperBound else { return nil }
+
+        let midIndex = range.lowerBound + ((range.upperBound - range.lowerBound) / 2)
+        let midValue = sortedArray[midIndex]
+
+        if midValue < searchValue {
+            return indexOfRecursive(sortedArray: sortedArray, searchValue: searchValue, range: midIndex+1..<range.upperBound)
+        } else if midValue > searchValue {
+            return indexOfRecursive(sortedArray: sortedArray, searchValue: searchValue, range: range.lowerBound..<midIndex)
+        } else {
+            return midIndex
+        }
     }
 
     static func indexOfIterative<T: Comparable>(sortedArray: [T], searchValue: T) -> Int? {
+        var lowerBound = 0
+        var upperBound = sortedArray.count
+
+        while lowerBound < upperBound {
+            let midIndex = lowerBound + ((upperBound - lowerBound) / 2)
+            let midValue = sortedArray[midIndex]
+
+            if midValue < searchValue {
+                lowerBound = midIndex + 1
+            } else if midValue > searchValue {
+                upperBound = midIndex
+            } else {
+                return midIndex
+            }
+        }
 
         return nil
     }
--- DemoAppTests/BinarySearchTests.swift
+++ DemoAppTests/BinarySearchTests.swift
@@ -14,11 +14,53 @@ class BinarySearchTests: XCTestCase {
     let sortedIntArray = [1, 3, 4, 7, 8, 9, 10, 14, 16, 17, 18, 20, 25, 27, 33, 34, 35, 40, 42, 43]
 
     func test_indexOfRecursive() {
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 1) == 0)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 3) == 1)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 4) == 2)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 7) == 3)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 8) == 4)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 9) == 5)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 10) == 6)
         XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 14) == 7)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 16) == 8)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 17) == 9)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 18) == 10)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 20) == 11)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 25) == 12)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 27) == 13)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 33) == 14)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 34) == 15)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 35) == 16)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 40) == 17)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 42) == 18)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 43) == 19)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: sortedIntArray, searchValue: 5) == nil)
+        XCTAssert(BinarySearch.indexOfRecursive(sortedArray: [], searchValue: 1) == nil)
     }
 
     func test_indexOfIterative() {
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 1) == 0)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 3) == 1)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 4) == 2)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 7) == 3)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 8) == 4)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 9) == 5)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 10) == 6)
         XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 14) == 7)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 16) == 8)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 17) == 9)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 18) == 10)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 20) == 11)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 25) == 12)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 27) == 13)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 33) == 14)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 34) == 15)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 35) == 16)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 40) == 17)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 42) == 18)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 43) == 19)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: sortedIntArray, searchValue: 5) == nil)
+        XCTAssert(BinarySearch.indexOfIterative(sortedArray: [], searchValue: 5) == nil)
     }
 
 }
