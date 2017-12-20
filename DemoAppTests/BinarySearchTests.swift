b08312acea27cad47a92db4932c667710eeb415d
dwall85 <dougwall85@gmail.com>
2017-12-20 10:13:15 -0500

    Added unit tests for recursiveBinarySearch and iterativeBinarySearch

---
+++ DemoAppTests/BinarySearchTests.swift
@@ -0,0 +1,24 @@
+//
+//  BinarySearchTests.swift
+//  DemoAppTests
+//
+//  Created by Douglas Wall on 12/19/17.
+//  Copyright © 2017 DevDoug. All rights reserved.
+//
+
+import XCTest
+@testable import DemoApp
+
+class BinarySearchTests: XCTestCase {
+
+    let sortedIntArray = [1, 3, 4, 7, 8, 9, 10, 14, 16, 17, 18, 20, 25, 27, 33, 34, 35, 40, 42, 43]
+
+    func test_recursiveBinarySearch() {
+        XCTAssert(BinarySearch.recursiveSearch(sortedArray: sortedIntArray, searchValue: 14) == 7)
+    }
+
+    func test_iterativeBinarySearch() {
+        XCTAssert(BinarySearch.iterativeSearch(sortedArray: sortedIntArray, searchValue: 14) == 7)
+    }
+
+}
